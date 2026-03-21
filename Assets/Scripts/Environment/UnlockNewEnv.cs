51e8e542b74efe33cf252ef0827a940b621cb0c5
Alejandro Hernández Calero <alehdzclr@gmail.com>
2026-03-21 15:09:47 +0100

    Monedas Colocadas

--- Assets/Scripts/Environment/UnlockNewEnv.cs
+++ Assets/Scripts/Environment/UnlockNewEnv.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Environment
@@ -5,6 +6,11 @@ namespace Environment
     public class UnlockNewEnv : MonoBehaviour
     {
         [SerializeField] private GameObject unlockThisThing;
+
+        private void Awake()
+        {
+            unlockThisThing.SetActive(false);
+        }
 
         private void OnDisable()
         {
