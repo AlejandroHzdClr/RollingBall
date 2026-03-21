12b41b927f8d839859c2794064df60d433bd7f60
Alejandro Hernández Calero <alehdzclr@gmail.com>
2026-03-21 11:13:16 +0100

    Sonido Moneda y rotacion

--- Assets/Scripts/Environment/Coins.cs
+++ Assets/Scripts/Environment/Coins.cs
@@ -1,18 +1,39 @@
+using System;
 using Player;
 using UnityEngine;
 
 namespace Environment
 {
-    public class Coins : PlayerSystem
+    public class Coins : MonoBehaviour
     {
         [SerializeField] private int pointNum;
+        [SerializeField] private float velocidadRotacion;
+
+        private AudioSource audioComp;
+
+        private void Start()
+        {
+            audioComp = GetComponent<AudioSource>();
+        }
+
+        void Update()
+        {
+            transform.Rotate((Vector3.forward * (velocidadRotacion * Time.deltaTime)));
+        }
+
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 PlayerMain player = other.GetComponent<PlayerMain>();
                 player.Points += pointNum;
-                Destroy(gameObject);
+                audioComp.Play();
+                foreach (var col in GetComponentsInChildren<Collider>())
+                    col.enabled = false;
+                foreach (var mesh in GetComponentsInChildren<MeshRenderer>())
+                    mesh.enabled = false;
+                Destroy(gameObject, audioComp.clip.length);
             }
         }
     }
